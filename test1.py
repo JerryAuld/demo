afbf36e69bffa93cdfec2b8ab96979ef21563978
JerryAuld <85122295+JerryAuld@users.noreply.github.com>
2021-06-01 10:57:34 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
+from pathlib import Path
+
 print("Running the Ashboarday Updater in python.")
+
+p = Path('./')
+
+# All subdirectories in the current directory, not recursive.
+# [f for f in p.iterdir() if f.is_dir()]
+
+subfolders = [ f.path for f in os.scandir('./') if f.is_dir() ]
+print(subfolders)
