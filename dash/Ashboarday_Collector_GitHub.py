e8b2661be87b65271697a83429c531052852cd66
JerryAuld <85122295+JerryAuld@users.noreply.github.com>
2021-06-05 11:48:04 -0600

    Update Ashboarday_Collector_GitHub.py

--- dash/Ashboarday_Collector_GitHub.py
+++ dash/Ashboarday_Collector_GitHub.py
@@ -27,15 +27,15 @@ def getLatestDate(folder):
   kids = 0
   files = [ file.path for file in os.scandir(folder) ]
   for f in files:
-  if file.is_dir():
-    kids += 1
-	else:
-		fcount += 1
-		(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(f)
-		thisdate = datetime.fromtimestamp(mtime)
-		fsize += size
-		if thisdate < latedate:
-		  latedate = thisdate
+    if file.is_dir():
+      kids += 1
+    else:
+      fcount += 1
+      (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(f)
+      thisdate = datetime.fromtimestamp(mtime)
+      fsize += size
+      if thisdate < latedate:
+        latedate = thisdate
       
   if fcount == 0:
     return "2009-01-01", fcount, fsize, kids
@@ -47,28 +47,26 @@ def getFolderData(folder, parent, order, level);
 
   if folder.is_dir():
 		
-		# Get the folder data:
+    # Get the folder data:
     result = getLatestDate(folder)
 		
-		# We must create this folder as a node on the dashboard before continuing, so we know the parent node ID for any children:
+    # We must create this folder as a node on the dashboard before continuing, so we know the parent node ID for any children:
     nID = urllib.request.urlopen(dashapi+"?p=" + parent + "&n=" + urllib.parse.quote_plus(folder) +"&f=" + str(result[1]) + "&l=" + str(level) + "&o=" + order + "&k=" + result[3] + "&s=GitHub&d=" + urllib.parse.quote_plus(result[0]) + "&z=" + str(result[2]) + "&i=" + urllib.parse.quote_plus(folder)).read()
 		
-		# If we are not at depth and up to our width, process the child folders of this folder:
-		subfolders = [ f.path for f in os.scandir(folder) if f.is_dir() ]
-		iOrder = 0
-		level += 1
+    # If we are not at depth and up to our width, process the child folders of this folder:
+    subfolders = [ f.path for f in os.scandir(folder) if f.is_dir() ]
+    iOrder = 0
+    level += 1
     if level < depth:
       for f in subfolders:
         if f.is_dir():
-					iOrder += 1
+          iOrder += 1
           if iOrder <= width:
-						getFolderData(folder."/".f, nID, $iOrder, level)
-		
-		return true
-	else:
-		return false
-	
-	
+            getFolderData(folder."/".f, nID, $iOrder, level)
+            return true
+          else:
+            return false
+
 #-----------------  MAIN   -----------------------------
 
 print("Python :: Running the Ashboarday Collector.")
@@ -83,25 +81,24 @@ if os.path.exists('dash/ashboarday.c.json'):
   root = jsondata['Start']
   width = jsondata['Width']
   depth = jsondata['Depth']
-  
+
   f.close
-  
+
   # Position ourselves at the start folder. We do not collect this folder, just its children.
   if root.is_dir():
 	
-	# Get all subdirectories in the current directory:
-	subfolders = [ f.path for f in os.scandir(root) if f.is_dir() ]
-	iOrder = 0
+    # Get all subdirectories in the current directory:
+    subfolders = [ f.path for f in os.scandir(root) if f.is_dir() ]
+    iOrder = 0
 	
-	for f in subfolders:
-    if f.is_dir():
-			iOrder += 1
-			getFolderData(root."/".f, 0, $iOrder, 1)
+    for f in subfolders:
+      if f.is_dir():
+        iOrder += 1
+        getFolderData(root."/".f, 0, $iOrder, 1)
+        print("SUCCESS: processing complete.")
 	
-	print("SUCCESS: processing complete.")
   else:
-	print("ERROR: specified root is not a directory.")
-    
+    print("ERROR: specified root is not a directory.")
 else:
   print("ERROR: json guidance file is not found. Please place in the same folder as this script.")
 
