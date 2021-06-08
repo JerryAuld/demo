887713cd66660a249d502bedd5703f732f98b5c3
JerryAuld <85122295+JerryAuld@users.noreply.github.com>
2021-06-08 11:13:00 -0600

    Update Ashboarday_Collector_GitHub.py

--- dash/Ashboarday_Collector_GitHub.py
+++ dash/Ashboarday_Collector_GitHub.py
@@ -56,7 +56,9 @@ def getFolderData(folder, parent, order, level):
   sFolderPath = urllib.parse.quote_plus(folder.path)
   sFolderName = urllib.parse.quote_plus(folder.name)
   print("Sending "+folder.name+" ("+folder.path+")")
-  nID = urllib.request.urlopen(dashapi+"&p=" + str(parent) + "&n=" + sFolderName +"&f=" + str(result[1]) + "&l=" + str(level) + "&o=" + str(order) + "&k=" + str(result[3]) + "&s=GitHub&d=" + urllib.parse.quote_plus(result[0]) + "&z=" + str(result[2]) + "&i=" + sFolderPath).read().decode('latin1')
+  dashURL = dashapi+"&p=" + str(parent) + "&n=" + sFolderName +"&f=" + str(result[1]) + "&l=" + str(level) + "&o=" + str(order) + "&k=" + str(result[3]) + "&s=GitHub&d=" + urllib.parse.quote_plus(result[0]) + "&z=" + str(result[2]) + "&i=" + sFolderPath
+  print(dashurl)
+  nID = urllib.request.urlopen(dashurl).read().decode('latin1')
   if nID.isnumeric():
     # If we are not at depth and up to our width, process the child folders of this folder:
     order = 0
